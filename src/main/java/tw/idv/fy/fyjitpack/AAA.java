759d7d310c7187b1e4117173f604bc3a8207307a
Frey <tw-Frey@users.noreply.github.com>
2018-08-27 16:52:16 +0800

    let rootProject be app module
    Signed-off-by: Frey <tw-Frey@users.noreply.github.com>

---
+++ src/main/java/tw/idv/fy/fyjitpack/AAA.java
@@ -0,0 +1,12 @@
+package tw.idv.fy.fyjitpack;
+
+import tw.idv.fy.javalibrary.FYClass;
+
+public class AAA extends FYClass {
+    static {
+        FYClass.main(null);
+    }
+    void main(){
+        AAA.main(null);
+    }
+}
